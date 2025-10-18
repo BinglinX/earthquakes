a2a1bfd386927543938d74e11942fa2deda8e1ce
BinglinX <zcbtbx0@ucl.ac.uk>
2025-10-18 22:12:30 +0100

    Fixed the error in get_location function It now returns 2 coordinates instead of 1

--- earthquakes.py
+++ earthquakes.py
@@ -41,7 +41,7 @@ def get_magnitude(earthquake):
 def get_location(earthquake):
     """Retrieve the latitude and longitude of an earthquake item."""
     # There are three coordinates, but we don't care about the third (altitude)
-    return earthquake['geometry']['coordinates'][0:1]
+    return earthquake['geometry']['coordinates'][0:2]
 
 
 def get_maximum(data):
